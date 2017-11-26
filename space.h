b7def96567680bbfeaad96d8ecaaa35cb75023c8
Klaas <nebuhr@encowayhb.lokal>
2017-11-26 20:07:46 +0100

    Fiddeling around with the constants

--- space.h
+++ space.h
@@ -3,18 +3,18 @@
 #include <stdint.h>
 
 typedef struct{
-  uint8_t x, y, vx, vy;
-  uint8_t exploding;
+  uint8_t x, y;
+  uint8_t exploding, vx, vy;
 } KlingonShip;
 
 typedef struct {
-  uint8_t x, y, vx, vy, h, w,shield_dx, shield_dy  ;
-  uint8_t exploding, shielded, damage;
-  uint16_t score;
+  uint8_t x, y, score;
+  uint8_t exploding, shielded, damage, vx, vy, h, w,shield_dx, shield_dy;
 } Discovery;
 
 typedef struct {
-  uint8_t x, y, w, h, isActive, vx;
+  uint8_t x, y;
+  uint8_t w, h, isActive, vx;
 } Missile;
 
 #endif
